8f4108646e30c08434fed9e6d76a54e82693727c
admin <admin@DESKTOP-82POU4M>
2021-01-01 14:41:50 +0530

    Changed server port 9090min place of 8080 using properties file

---
+++ HelloWordSpringBootServerPort9090/src/main/java/com/nttdata/app/HelloWordSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordSpringBootApplication.class, args);
+	}
+
+}
